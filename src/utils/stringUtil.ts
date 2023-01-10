d255ef31a0bfb9b63e3e3b1b25137cc4c99f07a2
Yslan Ramos <yslanramos.r@gmail.com>
2023-01-10 10:27:34 -0300

    feat: convert stringUtil to ts

---
+++ src/utils/stringUtil.ts
@@ -0,0 +1,15 @@
+export const slugify = (title: string): string => {
+  let slugArr = [];
+
+  for (let i = 0; i < title.length; i++) {
+    if (i >= 30) break;
+
+    let char = title[i].toLowerCase();
+    if (char >= 'a' && char <= 'z') slugArr.push(char);
+    else slugArr.push('-');
+  }
+
+  return slugArr.join('');
+};
+
+/* console.log(slugify("This is a really really looooooong title here.")) */
